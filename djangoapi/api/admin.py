977ea8cfcab031dd1d5f72448111162e46fe4236
Roman Grubic <grubic.roman@gmail.com>
2020-05-27 16:02:19 +0000

    Added HTML to admin panel. Issue with Heroku fixed.

--- djangoapi/api/admin.py
+++ djangoapi/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Python, JavaScript
+from .models import Python, JavaScript, HTML
 
 
 # Register your models here.
 admin.site.register(Python)
 admin.site.register(JavaScript)
+admin.site.register(HTML)
